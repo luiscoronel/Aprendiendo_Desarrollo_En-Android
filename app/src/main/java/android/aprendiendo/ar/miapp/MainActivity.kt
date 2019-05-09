c0f41a71a82ac7991326c047addab0a2a3f50a3a
Luis Coronel <luisito024@gmail.com>
2019-05-09 10:39:10 -0300

    Adding to the layout activity_main.xml: - TextView - EditText
    Add to the MainActivity function of send with Button

--- app/src/main/java/android/aprendiendo/ar/miapp/MainActivity.kt
+++ app/src/main/java/android/aprendiendo/ar/miapp/MainActivity.kt
@@ -3,20 +3,36 @@ package android.aprendiendo.ar.miapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    //Declare var btEnviar
     var btEnviar: Button? = null
+    var etIngresar:EditText? = null
+    var tvMostrar:TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btEnviar = findViewById(R.id.btEnviar)
+        etIngresar = findViewById(R.id.etIngresar)
+        tvMostrar = findViewById(R.id.tvMostrar)
+
+
 
         btEnviar?.setOnClickListener{
-            Toast.makeText(this, "Se acciono el boton enviar", Toast.LENGTH_SHORT).show()
+
+            val texto = etIngresar?.text
+
+            //Show text for screen
+            tvMostrar?.setText(texto)
+
+            //Test with Toast
+            //Toast.makeText(this, "Se acciono el boton enviar", Toast.LENGTH_SHORT).show()
         }
 
 
