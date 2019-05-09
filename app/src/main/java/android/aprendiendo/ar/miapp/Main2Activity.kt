33d380eb2b853802cd4d06d2e56a69bdf59c8ae6
Luis Coronel <luisito024@gmail.com>
2019-05-09 11:07:52 -0300

    Fixes to the file strings.xml Adding activity and activity_main2.xml: Main2Activity.kt activity_main2.xml

---
+++ app/src/main/java/android/aprendiendo/ar/miapp/Main2Activity.kt
@@ -0,0 +1,12 @@
+package android.aprendiendo.ar.miapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
