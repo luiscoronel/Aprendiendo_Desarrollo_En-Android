e9a2939d42fd7626308a1b1a9fc8c30cd969403c
Luis Coronel <luisito024@gmail.com>
2019-05-09 10:13:55 -0300

    Add implementation of button Enviar in MainActivity.kt

--- app/src/main/java/android/aprendiendo/ar/miapp/MainActivity.kt
+++ app/src/main/java/android/aprendiendo/ar/miapp/MainActivity.kt
@@ -2,11 +2,23 @@ package android.aprendiendo.ar.miapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var btEnviar: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btEnviar = findViewById(R.id.btEnviar)
+
+        btEnviar?.setOnClickListener{
+            Toast.makeText(this, "Se acciono el boton enviar", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
